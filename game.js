ce46dc89d812a64ca01c212ec90b90cc02cb7d8e
Anastasiya Khikhnal <a.khikhnal@gmail.com>
2019-07-01 13:27:33 +0300

    Create and describe canvas

--- game.js
+++ game.js
@@ -10,3 +10,15 @@ var requestAnimFrame = (function () {
       window.setTimeout(callback, 1000 / 60);
     };
 })();
+
+// Игровая среда CANVAS и её размеры
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+// var viewportWidth = window.innerWidth;
+// var viewportHeight = window.innerHeight;
+canvas.width = 800;
+canvas.height = 500;
+
+canvas.style.position = 'absolute';
+canvas.style.left = 0;
+canvas.style.top = 0;
