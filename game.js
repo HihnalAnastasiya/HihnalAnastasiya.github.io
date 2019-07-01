70e0c5ff445e8d54058c3b7c97537a8c38bb6fd1
Anastasiya Khikhnal <a.khikhnal@gmail.com>
2019-07-01 13:30:54 +0300

    Write image preloading function

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ var requestAnimFrame = (function () {
     };
 })();
 
+
 // Игровая среда CANVAS и её размеры
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
@@ -22,3 +23,25 @@ canvas.height = 500;
 canvas.style.position = 'absolute';
 canvas.style.left = 0;
 canvas.style.top = 0;
+
+
+// предзагруузка изображений
+var preloadedImagesH = {};
+
+function preloadImage(img) {
+  // если такое изображение уже предзагружалось - ничего не делаем
+  if (img in preloadedImagesH) {
+    return;
+  }
+  // предзагружаем - создаём невидимое изображение
+  var Img = new Image();
+  Img.src = img;
+  // запоминаем, что изображение уже предзагружалось
+  preloadedImagesH[img] = true;
+}
+
+preloadImage( 'img/bg01.jpg');
+preloadImage('img/ufo02.gif');
+preloadImage('img/cakes/cake01.png');
+preloadImage('img/cakes/cake02.png');
+preloadImage('img/cakes/cake03.png');
